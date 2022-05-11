017953c08204e0bc0f3cd9ad60ac7498c38393f9
Faizan Saleem <faizan.saleem@redbuffer.net>
2022-05-12 01:26:09 +0500

    initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to recommend floor plans for a plot of certain dimensions',
+    author='RB_Labs',
+    license='MIT',
+)
